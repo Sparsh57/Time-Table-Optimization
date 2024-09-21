d5f55116a50da59b281648ceebc2cd25c5357b76
Vatsalya Betala <vatsalyabetala@gmail.com>
2024-09-21 18:07:10 +0530

    Fixed Directory Error

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pandas as pd
 
 def load_data():
-    path_prefix = './data/'  # Adjust this if your directory structure is different
+    path_prefix = './retrieved_data/'  # Adjust this if your directory structure is different
     df_registration = pd.read_csv(path_prefix + 'Student_Registration.csv')
     df_faculty_pref = pd.read_csv(path_prefix + 'Faculty_Pref.csv')
     return df_registration, df_faculty_pref
