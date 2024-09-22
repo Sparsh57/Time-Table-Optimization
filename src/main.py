8489edeb99efc05cdcb73c239d48d7b70312ee66
Sparsh Makharia <sparshmakharia@gmail.com>
2024-09-22 22:03:48 +0530

    Dynamic import to database_retrieval.py so that the file is not stored locally

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@ from data_preprocessing import load_data, merge_data, prepare_student_course_map
 from utilities import faculty_busy_slots, create_course_dictionary
 from schedule_model import schedule_courses
 from conflict_checker import check_conflicts
+from database_management.database_retrieval import registration_data, faculty_pref
 
 # Load data
 df_registration, df_faculty_pref = load_data()
-df_merged = df_registration
+df_merged = registration_data()
 student_course_map = prepare_student_course_map(df_merged)
 course_professor_map = create_course_professor_map(df_merged)
-professor_busy_slots = faculty_busy_slots(df_faculty_pref)
+professor_busy_slots = faculty_busy_slots(faculty_pref())
 
 # Prepare courses and scheduleS
 courses = create_course_dictionary(student_course_map, course_professor_map, professor_busy_slots)
