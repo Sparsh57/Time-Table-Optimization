d24b0ee55ac039a7757d5d9f57554e23731632d5
Vatsalya Betala <vatsalyabetala@gmail.com>
2024-09-22 23:10:33 +0530

    Removed local imports of csv files generated by sql queries

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,14 +1,5 @@
 import pandas as pd
 
-import pandas as pd
-
-def load_data():
-    path_prefix = './retrieved_data/'  # Adjust this if your directory structure is different
-    df_registration = pd.read_csv(path_prefix + 'Student_Registration.csv')
-    df_faculty_pref = pd.read_csv(path_prefix + 'Faculty_Pref.csv')
-    return df_registration, df_faculty_pref
-
-
 def merge_data(df_registration, df_courses):
     df_merged = pd.merge(df_registration, df_courses, left_on='G CODE', right_on='Course code', how='left')
     df_merged['Professor'] = df_merged['Faculty Name']
--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from data_preprocessing import load_data, merge_data, prepare_student_course_map, create_course_professor_map
+from data_preprocessing import merge_data, prepare_student_course_map, create_course_professor_map
 from utilities import faculty_busy_slots, create_course_dictionary
 from schedule_model import schedule_courses
 from conflict_checker import check_conflicts
 from database_management.database_retrieval import registration_data, faculty_pref
 
 # Load data
-df_registration, df_faculty_pref = load_data()
 df_merged = registration_data()
 student_course_map = prepare_student_course_map(df_merged)
 course_professor_map = create_course_professor_map(df_merged)
